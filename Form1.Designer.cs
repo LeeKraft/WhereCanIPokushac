2a5e5ae3555d94c35601af6b42ad8ce39babd17b
LeeKraft <zhenya.kraft.2000@mail.ru>
2020-05-31 20:49:07 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace karpan
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gmap = new GMap.NET.WindowsForms.GMapControl();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // gmap
+            // 
+            this.gmap.Bearing = 0F;
+            this.gmap.CanDragMap = true;
+            this.gmap.EmptyTileColor = System.Drawing.Color.Navy;
+            this.gmap.GrayScaleMode = false;
+            this.gmap.HelperLineOption = GMap.NET.WindowsForms.HelperLineOptions.DontShow;
+            this.gmap.LevelsKeepInMemmory = 5;
+            this.gmap.Location = new System.Drawing.Point(15, 13);
+            this.gmap.Margin = new System.Windows.Forms.Padding(1, 3, 1, 3);
+            this.gmap.MarkersEnabled = true;
+            this.gmap.MaxZoom = 2;
+            this.gmap.MinZoom = 2;
+            this.gmap.MouseWheelZoomEnabled = true;
+            this.gmap.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionAndCenter;
+            this.gmap.Name = "gmap";
+            this.gmap.NegativeMode = false;
+            this.gmap.PolygonsEnabled = true;
+            this.gmap.RetryLoadTile = 0;
+            this.gmap.RoutesEnabled = true;
+            this.gmap.ScaleMode = GMap.NET.WindowsForms.ScaleModes.Integer;
+            this.gmap.SelectedAreaFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(65)))), ((int)(((byte)(105)))), ((int)(((byte)(225)))));
+            this.gmap.ShowTileGridLines = false;
+            this.gmap.Size = new System.Drawing.Size(737, 544);
+            this.gmap.TabIndex = 0;
+            this.gmap.Zoom = 0D;
+            this.gmap.Load += new System.EventHandler(this.gMapControl1_Load);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(880, 13);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(1, 3, 1, 3);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(205, 22);
+            this.textBox1.TabIndex = 1;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(768, 62);
+            this.checkBox1.Margin = new System.Windows.Forms.Padding(1, 3, 1, 3);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(66, 21);
+            this.checkBox1.TabIndex = 2;
+            this.checkBox1.Text = "Кафе";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(878, 62);
+            this.checkBox2.Margin = new System.Windows.Forms.Padding(1, 3, 1, 3);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(93, 21);
+            this.checkBox2.TabIndex = 3;
+            this.checkBox2.Text = "Ресторан";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(998, 62);
+            this.checkBox3.Margin = new System.Windows.Forms.Padding(1, 3, 1, 3);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(93, 21);
+            this.checkBox3.TabIndex = 4;
+            this.checkBox3.Text = "Столовая";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.Location = new System.Drawing.Point(758, 167);
+            this.button1.Margin = new System.Windows.Forms.Padding(1, 3, 1, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(345, 32);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Найти";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.richTextBox1.Location = new System.Drawing.Point(758, 241);
+            this.richTextBox1.Margin = new System.Windows.Forms.Padding(1, 3, 1, 3);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(347, 313);
+            this.richTextBox1.TabIndex = 7;
+            this.richTextBox1.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(767, 16);
+            this.label1.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(107, 17);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Введите район";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(862, 108);
+            this.label2.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(132, 17);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Количество людей";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(878, 128);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(1, 3, 1, 3);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 10;
+            this.textBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox2_KeyPress);
+            // 
+            // button2
+            // 
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button2.Location = new System.Drawing.Point(758, 206);
+            this.button2.Margin = new System.Windows.Forms.Padding(1, 3, 1, 3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(345, 29);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Очистить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1115, 566);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.gmap);
+            this.Name = "Form1";
+            this.Text = "Где поесть в Москве?";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private GMap.NET.WindowsForms.GMapControl gmap;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using GMap.NET;
+using GMap.NET.WindowsForms;
+using GMap.NET.WindowsForms.Markers;
+using Newtonsoft.Json;
+
+namespace karpan
+{
+    public partial class Form1 : Form
+    {
+        public class GeoData
+        {
+            public double[] Coordinates { get; set; }
+        }
+        
+        public class Object
+        {
+            public string Name { get; set; }
+            public string TypeObject { get; set; }
+            public string District { get; set; }
+            public string Address { get; set; }
+         
+            public int SeatsCount { get; set; }
+            public GeoData geoData { get; set; }
+
+        }
+
+        readonly List<Object> Elements;
+
+        public Form1()
+        {
+            InitializeComponent();
+            string path = "cafe.json";
+            var encoder = Encoding.GetEncoding(1251);
+            StreamReader r = new StreamReader(path, encoder);
+            string json = r.ReadToEnd();
+            Elements = JsonConvert.DeserializeObject<List<Object>>(json);
+            MessageBox.Show(
+                    "Данные успешно загружены",
+                    "Загрузка данных",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Information);
+            textBox2.Text = "0";
+            r.Dispose();
+        }
+
+        private void gMapControl1_Load(object sender, EventArgs e)
+        {
+            // Настройки для компонента GMap
+            gmap.Bearing = 0;
+            // Перетаскивание правой кнопки мыши
+            gmap.CanDragMap = true;
+            // Перетаскивание карты левой кнопкой мыши
+            gmap.DragButton = MouseButtons.Left;
+
+            gmap.GrayScaleMode = true;
+
+            // Все маркеры будут показаны
+            gmap.MarkersEnabled = true;
+            // Максимальное приближение
+            gmap.MaxZoom = 18;
+            // Минимальное приближение
+            gmap.MinZoom = 2;
+            // Курсор мыши в центр карты
+            gmap.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionWithoutCenter;
+
+            // Отключение нигативного режима
+            gmap.NegativeMode = false;
+            // Разрешение полигонов
+            gmap.PolygonsEnabled = true;
+            // Разрешение маршрутов
+            gmap.RoutesEnabled = true;
+            // Скрытие внешней сетки карты
+            gmap.ShowTileGridLines = false;
+            // При загрузке 10-кратное увеличение
+            gmap.Zoom = 10;
+
+            // Чья карта используется
+            gmap.MapProvider = GMap.NET.MapProviders.GMapProviders.GoogleMap;
+
+            // Загрузка этой точки на карте
+            GMap.NET.GMaps.Instance.Mode = GMap.NET.AccessMode.ServerOnly;
+            gmap.Position = new GMap.NET.PointLatLng(55.7522200, 37.6155600);
+
+            gmap.Overlays.Add(new GMapOverlay());
+        }
+
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char number = e.KeyChar;
+
+            if ((!Char.IsDigit(number))&& (!char.IsControl(number)))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int flag = 0;
+            gmap.Zoom = 13;
+
+            if (textBox1.Text == "")
+            {
+                MessageBox.Show(
+                    "Введите район", 
+                    "Ошибка", 
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+            }
+
+            if ((checkBox1.Checked != true) && (checkBox2.Checked != true) && (checkBox3.Checked != true))
+            {
+                MessageBox.Show(
+                    "Выберите тип заведения",
+                    "Ошибка",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+            }
+
+            foreach (var item in Elements)
+            {
+                if (checkBox1.Checked == true)
+                {
+                    if ((item.TypeObject == "кафе") && (item.SeatsCount > int.Parse(textBox2.Text)) && (item.District == textBox1.Text))
+                    {
+                        double lat = item.geoData.Coordinates[1];
+                        double lon = item.geoData.Coordinates[0];
+                        GMarkerGoogle mark = new GMarkerGoogle(new PointLatLng(lat, lon), GMarkerGoogleType.blue_small)
+                        {
+                            ToolTipText = item.Name
+                        };
+
+                        gmap.Overlays[0].Markers.Add(mark);
+                        richTextBox1.Text += item.Name + "\n" + item.Address + "\n" + "Кафе" + "\n" + "Количество мест: " + item.SeatsCount + "\n" + "\n";
+
+                        if (flag == 0)
+                        {
+                            flag = 1;
+                            gmap.Position = new GMap.NET.PointLatLng(item.geoData.Coordinates[1], item.geoData.Coordinates[0]);
+                        }
+                    }
+                }
+
+                if (checkBox2.Checked == true)
+                {
+                    if ((item.TypeObject == "ресторан") && (item.SeatsCount > int.Parse(textBox2.Text)) && (item.District == textBox1.Text))
+                    {
+                        double lat = item.geoData.Coordinates[1];
+                        double lon = item.geoData.Coordinates[0];
+                        GMarkerGoogle mark = new GMarkerGoogle(new PointLatLng(lat, lon), GMarkerGoogleType.yellow_small)
+                        {
+                            ToolTipText = item.Name
+                        };
+
+                        gmap.Overlays[0].Markers.Add(mark);
+                        richTextBox1.Text += item.Name + "\n" + item.Address + "\n" + "Ресторан" + "\n" + "Количество мест: " + item.SeatsCount + "\n" + "\n";
+
+                        if (flag == 0)
+                        {
+                            flag = 1;
+                            gmap.Position = new GMap.NET.PointLatLng(item.geoData.Coordinates[1], item.geoData.Coordinates[0]);
+                        }
+                    }
+                }
+
+                if (checkBox3.Checked == true)
+                {
+                    if ((item.TypeObject == "столовая") && (item.SeatsCount > int.Parse(textBox2.Text)) && (item.District == textBox1.Text))
+                    {
+                        double lat = item.geoData.Coordinates[1];
+                        double lon = item.geoData.Coordinates[0];
+                        GMarkerGoogle mark = new GMarkerGoogle(new PointLatLng(lat, lon), GMarkerGoogleType.white_small)
+                        {
+                            ToolTipText = item.Name
+                        };
+
+                        gmap.Overlays[0].Markers.Add(mark);
+                        richTextBox1.Text += item.Name + "\n" + item.Address + "\n" + "Столовая" + "\n" + "Количество мест: " + item.SeatsCount + "\n" + "\n";
+
+                        if (flag == 0)
+                        {
+                            flag = 1;
+                            gmap.Position = new GMap.NET.PointLatLng(item.geoData.Coordinates[1], item.geoData.Coordinates[0]);
+                        }
+                    }
+                }
+            }
+
+            if (richTextBox1.Text == "")
+            {
+                richTextBox1.Text = "Список пуст";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            gmap.Overlays[0].Clear();
+            richTextBox1.Text = "";
+        }
+    }
+}
